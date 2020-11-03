51c680812e61e778c909fb13dc55fd97ff0ae4b6
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2020-11-03 15:31:56 -0800

    run age input through cnosole correctly

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,42 @@ function Movie(movieTitle, time, age) {
   this.age = age;
 }
 
+
+
+
+
+/*
+// Movies Titles //
+
+"First-release"
+  1. The Sandlot
+  2. The Matrix
+  3. Avatar
+
+"Non first-release":
+  4. Star Wars: Random
+  5. Transformers
+
+
+// Discount Factors //
+
+  Before 12pm - Matinee (discounted)
+  After 12pm - Normal priced
+
+  Seniors 65+ - discounted
+
+  Non-"first-release" - discounted
+//  //
+*/
+
+
+
+
+$(document).ready(function() {
+  $("form#age-input").submit(function(event) {
+    event.preventDefault();
+    const inputtedAge = parseInt($("input#customer-age").val());
+    $("input#customer-age").val();
+    console.log(inputtedAge);
+  })
+})
